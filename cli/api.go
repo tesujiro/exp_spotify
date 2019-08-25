017a880cba045f1e58509d4d65f43bf67f7938c2
tesujiro <tesujiro@gmail.com>
2019-08-26 07:59:23 +0900

    add: proxy still not working...

--- cli/api.go
+++ cli/api.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 /*
@@ -17,35 +21,58 @@ type SpotifyAPI struct {
 }
 */
 
-func get(token string, endpoint string, params url.Values) ([]byte, error) {
+func get(token, endpoint string, params url.Values) ([]byte, error) {
+	return call(token, "GET", endpoint, params, nil)
+}
+
+func put(token string, endpoint string, params url.Values, body io.Reader) ([]byte, error) {
+	return call(token, "PUT", endpoint, params, body)
+}
+
+func call(token, method, endpoint string, params url.Values, body io.Reader) ([]byte, error) {
+
 	//fmt.Println("endpoint:", endpoint)
 	baseUrl, err := url.Parse(endpoint)
 	if err != nil {
 		return nil, err
 	}
-	// set query parameters
-	/*
-		params := url.Values{}
-		for k, v := range query {
-			params.Add(k, v)
-		}
-	*/
 
 	baseUrl.RawQuery = params.Encode() // Escape Query Parameters
 
-	req, err := http.NewRequest("GET", baseUrl.String(), nil)
+	req, err := http.NewRequest(method, baseUrl.String(), body)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Authorization", "Bearer "+token)
 
-	resp, err := http.DefaultClient.Do(req)
+	var client *http.Client
+	if os.Getenv("ProxyServer") == "" {
+		client = http.DefaultClient
+	} else {
+		proxyURL, err := url.Parse(os.Getenv("ProxyServer"))
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Printf("proxyURL=%v\n", proxyURL)
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+			Proxy: http.ProxyURL(proxyURL),
+		}
+		client = &http.Client{
+			Transport: transport,
+		}
+	}
+	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Printf("resp=%v\n", resp)
 		return nil, err
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode >= 400 {
+		fmt.Printf("response.Body=%#v\n", resp.Body)
 		return nil, fmt.Errorf("bad response status code %d", resp.StatusCode)
 	}
 	//fmt.Println("response status code ", resp.StatusCode)
